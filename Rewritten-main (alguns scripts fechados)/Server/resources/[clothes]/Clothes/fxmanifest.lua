b438447c29a4e9d449698dc5caa5fc6476421d6e
Philippe Oliveira <74221811+Solliiver@users.noreply.github.com>
2025-04-29 16:39:45 -0300

    teste

---
+++ Rewritten-main (alguns scripts fechados)/Server/resources/[clothes]/Clothes/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "bodacious"
+game "gta5"
+
+files {
+	"data/mp_f_freemode_01_mp_f_female_one.meta",
+	"data/mp_f_freemode_01_mp_f_female_two.meta",
+	"data/mp_m_freemode_01_mp_m_male_one.meta",
+	"data/mp_m_freemode_01_bhmhat1.meta",
+	"data/mp_f_freemode_01_bhmhat1.meta"
+}
+
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_female_one.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_female_two.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_m_freemode_01_mp_m_male_one.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_m_freemode_01_bhmhat1.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_bhmhat1.meta"
